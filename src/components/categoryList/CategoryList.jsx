951eb75590e4bc7ca0e027371e380478dcb26a64
MustafaKole <blackunwer0@gmail.com>
2024-01-11 11:13:10 +0300

    chore: update category list

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -4,9 +4,16 @@ import React from "react";
 import styles from "./categoryList.module.css";
 
 /* const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
-    cache: "no-store",
-  });
+  let res;
+  if (process.env.NODE_ENV === "development") {
+    res = await fetch("http://localhost:3000/api/categories", {
+      cache: "no-store",
+    });
+  } else {
+    res = await fetch("https://mkole.vercel.app/api/categories", {
+      cache: "no-store",
+    });
+  }
 
   if (!res.ok) {
     throw new Error("Failed");
@@ -40,49 +47,16 @@ const CategoryList = async () => {
           </Link>
         ))} */}
         {/* If you have prepared the database, you can delete the codes below and use the code above instead. */}
-        <Link href={"/"} className={`${styles.category} ${styles.culture}`}>
+        <Link href={"/"} className={`${styles.category} ${styles.style}`}>
           <Image
-            src={"/mountain.jpg"}
+            src={"/style.jpg"}
             alt=""
             width={32}
             height={32}
             className={styles.image}
             priority={true}
           />
-          Culture
-        </Link>
-        <Link href={"/"} className={`${styles.category} ${styles.travel}`}>
-          <Image
-            src={"/travel.jpg"}
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-            priority={true}
-          />
-          Travel
-        </Link>
-        <Link href={"/"} className={`${styles.category} ${styles.coding}`}>
-          <Image
-            src={"/coding.jpg"}
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-            priority={true}
-          />
-          Coding
-        </Link>
-        <Link href={"/"} className={`${styles.category} ${styles.fashion}`}>
-          <Image
-            src={"/fashion.jpg"}
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-            priority={true}
-          />
-          Fashion
+          Style
         </Link>
         <Link href={"/"} className={`${styles.category} ${styles.food}`}>
           <Image
@@ -95,16 +69,50 @@ const CategoryList = async () => {
           />
           Food
         </Link>
-        <Link href={"/"} className={`${styles.category} ${styles.style}`}>
+        <Link href={"/"} className={`${styles.category} ${styles.gaming}`}>
           <Image
-            src={"/style.jpg"}
+            src={"/gaming.jpg"}
             alt=""
             width={32}
             height={32}
             className={styles.image}
             priority={true}
           />
-          Style
+          Gaming
+        </Link>
+        <Link href={"/"} className={`${styles.category} ${styles.travel}`}>
+          <Image
+            src={"/travel.jpg"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+            priority={true}
+          />
+          Travel
+        </Link>
+
+        <Link href={"/"} className={`${styles.category} ${styles.coding}`}>
+          <Image
+            src={"/coding.jpg"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+            priority={true}
+          />
+          Coding
+        </Link>
+        <Link href={"/"} className={`${styles.category} ${styles.culture}`}>
+          <Image
+            src={"/culture.jpg"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+            priority={true}
+          />
+          Culture
         </Link>
       </div>
     </div>
